0674287a6ea1344baae29c106ca870f93c3b9b16
abbappii <bappi142434@gmail.com>
2021-03-21 17:10:22 +0600

    first upload full project

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/bappi/Class/Projects bohhubrihi/All Projects and Practice/Django part/My Ecommerce/My Ecommerce/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
